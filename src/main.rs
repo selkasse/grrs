e5d3ca5c87f31db48b7da28c48055c52f37a12c4
Sharif Elkassed <selkasse@gmu.edu>
2024-05-27 16:26:52 -0400

    commit before using 'anyhow'crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+//use anyhow::{Context, Result};
 
 /// Search for a patern in a file and display the lines that contain it
 #[derive(Parser)]
@@ -6,15 +10,29 @@ struct Cli {
     /// The pattern to look for
     pattern: String,
     /// The path to the file to read
-    // PathBuf is like a String, but for file systems paths (works cross-platform)
     path: std::path::PathBuf, 
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
 
     // the parse() method is provided by the clap::Parser trait
     // don't use this function outside of main()!
     let args = Cli::parse();
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    let file = File::open(&args.path)?;
+        //.with_context(|| format!("could not read file `{}`", args.path.display()));
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines(){
+
+        // Handle potential errors from lines iterator
+        let line = line?;
+        if line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
+    
 }
