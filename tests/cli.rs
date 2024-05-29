c1e68a7ecaebdb11f6972ec505ca386942e5fd86
Sharif Elkassed <selkasse@gmu.edu>
2024-05-29 09:49:20 -0400

    testing complete

--- tests/cli.rs
+++ tests/cli.rs
@@ -32,6 +32,23 @@ fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+fn no_match_found() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("blorp").arg(file.path());
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    Ok(())
+}
+
+#[test]
 fn empty_string_as_pattern() -> Result<(), Box<dyn std::error::Error>> {
     let file = assert_fs::NamedTempFile::new("sample.txt")?;
     file.write_str("A space\nThis is space test\nMore space\nAnother space")?;
@@ -41,6 +58,52 @@ fn empty_string_as_pattern() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("pattern must not be empty"));
+
+    Ok(())
+}
+
+#[test]
+fn missing_pattern() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("This\nWon't\nMatter\nAnother space")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg(file.path());
+
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "the following required arguments were not provided",
+    ));
+
+    Ok(())
+}
+
+#[test]
+fn missing_path() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("This\nWon't\nMatter\nAnother space")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("testpattern");
+
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "the following required arguments were not provided",
+    ));
+
+    Ok(())
+}
+
+#[test]
+fn missing_pattern_and_path() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("This\nWon't\nMatter\nAnother space")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "the following required arguments were not provided",
+    ));
 
     Ok(())
 }
